6e2dd9f8cc09e240d742e665dfcdf5cb60f055cc
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-05-28 23:16:46 +0530

    deshaw oa problem easy version bs.

--- src/OnlineAssement/BinarySearch/BinarySearch.java
+++ src/OnlineAssement/BinarySearch/BinarySearch.java
@@ -41,4 +41,55 @@ public class BinarySearch {
 //        }
 //        return true;
 //    }
+
+    public static int question_deshaw(int[][] matrix,int n,int m){
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                max = Math.max(max,matrix[i][j]);
+            }
+        }
+        int ans = max;
+        for(int i=0;i<=max;i++){
+            if(isPossible(i,matrix,n,m)){
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+    public static boolean isPossible(int target,int[][] matrix,int n,int m){
+
+        int[][] prefix = new int[n][m];
+        int[][] suffix = new int[n][m];
+        for(int i=0;i<n;i++){
+            prefix[i][0]  = matrix[i][0];
+            suffix[i][m-1] = matrix[i][m-1];
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+             prefix[i][j] = Math.max(prefix[i][j-1],matrix[i][j]);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=n-2;j>=0;j--){
+                suffix[i][j] = Math.max(suffix[i][j+1],matrix[i][j]);
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i<m;i++){
+            int max = Integer.MIN_VALUE;
+            for(int j=0;j<n-1;j++){
+                int val1 = prefix[j][i-1];
+                int val2 = suffix[j][i+1];
+                max = Math.max(max,Math.max(val1,val2));
+                min = Math.min(max,min);
+            }
+        }
+
+        return (min==target);
+    }
 }
