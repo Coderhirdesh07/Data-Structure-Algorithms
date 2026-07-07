50a426e2cc64c6fee1e05ac51d549bbd0db42c2d
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-07-07 20:51:14 +0530

    did completely phonepe oa problem with myself.

--- src/dsacourse851/DynamicProgramming/UnderstandingSimpleDp.java
+++ src/dsacourse851/DynamicProgramming/UnderstandingSimpleDp.java
@@ -473,14 +473,17 @@ public class UnderstandingSimpleDp {
             Arrays.fill(dp[i],Integer.MIN_VALUE);
         }
 
-        dp[1][0] = arr[1] + arr[0];
-        dp[2][0] = arr[2]+arr[1];
-        dp[2][1] = arr[2]+arr[1]+arr[0];
-        for(int i=3;i<n;i++){
+        dp[1][0] = arr[0] + arr[1];
+        dp[2][0] = Math.max(arr[0]+arr[1],arr[1]+arr[2]);
+        dp[2][1] = arr[2]+arr[1] + arr[0];
+        dp[3][0] = Math.max(arr[3]+arr[2],dp[2][0]);
+        dp[3][1] = Math.max(arr[3]+arr[2]+arr[1],dp[2][1]);
 
-            dp[i][0] = arr[i]+arr[i-1] + Math.max(dp[i-2][0],Math.max(dp[i-2][1],Math.max(dp[i-3][0],dp[i-3][1])));
-            if(i>=4){
-                dp[i][1] = arr[i]+arr[i-1]+arr[i-2] + Math.max(dp[i-3][0],Math.max(dp[i-3][1],Math.max(dp[i-4][0] ,dp[i-4][1])));
+        for(int i=4;i<n;i++){
+            dp[i][0] = arr[i]+arr[i-1] + Math.max(dp[i-3][0],dp[i-4][1]);
+            dp[i][1] = arr[i]+arr[i-1] + arr[i-2] + dp[i-4][0];
+            if(i>=5){
+                dp[i][1] = arr[i]+arr[i-1]+arr[i-2] + Math.max(dp[i-4][0],dp[i-5][1]);
             }
         }
         return Math.max(dp[n-1][0],dp[n-1][1]);
