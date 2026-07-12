cf31f38c4a38c2a954745218a1420f0a0d493cc9
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-07-12 23:35:59 +0530

    Solved prefix xor problem.

--- src/Tlecourse/Level2.java
+++ src/Tlecourse/Level2.java
@@ -1,7 +1,34 @@
 package Tlecourse;
 
+import java.util.Scanner;
+
 public class Level2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] queries = new int[m][2];
+        for(int i=0;i<m;i++){
+            queries[i][0] = sc.nextInt();
+            queries[i][1] = sc.nextInt();
+        }
+        int[] pXor = question_2(arr,n);
+        for(int i=0;i<m;i++){
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int res = pXor[r] ^ pXor[l-1];
+            System.out.println(res);
+        }
 
+    }
+    public static int[] question_2(int[] arr,int n){
+        int[] prefixMatrix = new int[n];
+        prefixMatrix[0] = arr[0];
+        for(int i=1;i<n;i++){
+            prefixMatrix[i] = prefixMatrix[i-1]^arr[i];
+        }
+        return prefixMatrix;
     }
 }
