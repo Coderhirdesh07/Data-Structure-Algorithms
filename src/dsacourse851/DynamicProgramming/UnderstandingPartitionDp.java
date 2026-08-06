cc61a77522dd09bdeff31c748821f9dbca2606c9
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-08-06 22:55:15 +0530

    some minor test cases.

--- src/dsacourse851/DynamicProgramming/UnderstandingPartitionDp.java
+++ src/dsacourse851/DynamicProgramming/UnderstandingPartitionDp.java
@@ -21,8 +21,10 @@ public class UnderstandingPartitionDp {
 //        }
         int[] a = {0,16 ,20 ,12 ,12, 8, 2, 3, 8, 17, 3, 14, 8};
         int[] b = {0,16, 17, 1, 2, 20, 17, 1, 3 ,9 ,8 ,10, 16};
+//        int[] arr= {3,1,4,6,5,2};
+        int[] arr= {2,1,4,3};
 
-        int ans = question_52("1023",1000);  // 1 2 3 4 , 12 3 4, 12 34,123 4 , 1 234
+        int ans = question_54(arr,arr.length);  // 1 2 3 4 , 12 3 4, 12 34,123 4 , 1 234
         System.out.println("This is the "+ans);
         // 5 3
         //1 2 1 3 5
@@ -335,18 +337,18 @@ public class UnderstandingPartitionDp {
 //      public static int question_53(){}
     public static int question_54(int[] arr,int n){
         int[] dp = new int[n];
-        dp[0] = 0;
-        for(int i=1;i<=n;i++){
+        dp[0] = 0 ;
+        for(int i=1;i<n;i++){
             int max = arr[i];
             for(int j=i-1;j>=0;j--){
                 boolean isGood = goodSubarray(arr,j,i);
-                if(isGood){
-                    dp[i] = 1 + dp[j-1];
+                if(isGood && j-1>=0){
+                    dp[i] = Math.max(dp[i],1 + dp[j]);
                 }
                 else dp[i] = Math.max(dp[i],1);
             }
         }
-        return dp[n];
+        return dp[n-1];
     }
     public static boolean goodSubarray(int[] arr,int start,int end){
         int ind = -1;
