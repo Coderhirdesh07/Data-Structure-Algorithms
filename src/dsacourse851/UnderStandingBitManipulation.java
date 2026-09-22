2c86a312b43b6aadf3f387f69659ee67bb2f373e
Hirdesh Keswani <hirdeshkeshwani2001@gmail.com>
2026-09-22 23:02:20 +0530

    solved some bruteforce problem.

--- src/dsacourse851/UnderStandingBitManipulation.java
+++ src/dsacourse851/UnderStandingBitManipulation.java
@@ -242,4 +242,25 @@ public class UnderStandingBitManipulation {
         }
         return val;
     }
+    public static int question_16_brute(int[] arr,int n){
+        int val = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                val+=arr[i]|arr[j];
+            }
+        }
+        return val;
+    }
+
+    public static int question_19_brute(int[] arr,int n){
+        int val = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++) {
+                    val += arr[i] ^ arr[j] ^ arr[k];
+                }
+            }
+        }
+        return val;
+    }
 }
